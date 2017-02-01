5bb5a27dada8a01d5ca7d1c7e96229b1b09dcc1d
Gary Ewan Park <gep13@gep13.co.uk>
2017-02-01 20:47:35 +0000

    AppVeyor Publish: 86aea5c108bf4459468cfba68642e214f6f322b3 (GH-14) Added Wyam Docs

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,98 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"EmailSettings",
+        content:"EmailSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"EmailResult",
+        content:"EmailResult",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"EmailAliases",
+        content:"EmailAliases",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"EmailProvider",
+        content:"EmailProvider",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailSettings',
+        title:"EmailSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailResult',
+        title:"EmailResult",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailAliases',
+        title:"EmailAliases",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailProvider',
+        title:"EmailProvider",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
