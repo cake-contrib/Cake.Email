01fd7889893f161b4aeffc98c727c27baded84ea
Jeremie Desautels <desautelsj@hotmail.com>
2017-10-02 13:47:59 -0700

    AppVeyor Publish: 5d2031b4d7548515e96b9b08bd6ee594500f6352 Merge branch 'release/0.5.4'

--- assets/js/searchIndex.js
+++ assets/js/searchIndex.js
@@ -34,6 +34,14 @@ var searchModule = function() {
 
     a({
         id:0,
+        title:"EmailSettings",
+        content:"EmailSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
         title:"EmailProvider",
         content:"EmailProvider",
         description:'',
@@ -41,7 +49,7 @@ var searchModule = function() {
     });
 
     a({
-        id:1,
+        id:2,
         title:"EmailAliases",
         content:"EmailAliases",
         description:'',
@@ -49,19 +57,17 @@ var searchModule = function() {
     });
 
     a({
-        id:2,
+        id:3,
         title:"EmailResult",
         content:"EmailResult",
         description:'',
         tags:''
     });
 
-    a({
-        id:3,
+    y({
+        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailSettings',
         title:"EmailSettings",
-        content:"EmailSettings",
-        description:'',
-        tags:''
+        description:""
     });
 
     y({
@@ -79,12 +85,6 @@ var searchModule = function() {
     y({
         url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailResult',
         title:"EmailResult",
-        description:""
-    });
-
-    y({
-        url:'/Cake.Email/Cake.Email/api/Cake.Email/EmailSettings',
-        title:"EmailSettings",
         description:""
     });
 
